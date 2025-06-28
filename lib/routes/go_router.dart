36183f7c93db6037c2485076a024bc71e2be9402
Maytton <maytton0000@gmail.com>
2025-06-28 16:33:06 -0300

    feat(go_router): Injeta LoginViewModel na rota /login

--- lib/routes/go_router.dart
+++ lib/routes/go_router.dart
@@ -1,6 +1,10 @@
 import 'package:desafio_flutter/routes/app_routes.dart';
+import 'package:desafio_flutter/viewmodels/login_view_model.dart';
 import 'package:desafio_flutter/views/login/login_view.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+import '../services/auth_service.dart' show AuthService;
 
 
 final GoRouter router = GoRouter(
@@ -9,7 +13,10 @@ final GoRouter router = GoRouter(
       GoRoute(
           path: '/login',
           name: RouteNames.login,
-          builder: (context, state) => const LoginView(),
+        builder: (context, state) => ChangeNotifierProvider(
+          create: (_) => LoginViewModel(authService: AuthService()),
+          child: const LoginView(),
+        ),
       ),
     ],
 );

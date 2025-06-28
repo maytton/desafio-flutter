1e05916c96275f9228c638188c92840a24437b93
Maytton <maytton0000@gmail.com>
2025-06-28 17:24:01 -0300

    feat: providers: Injeta AuthService no LoginViewModel

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,6 +1,8 @@
 import 'package:desafio_flutter/viewmodels/login_view_model.dart';
 import 'package:provider/provider.dart';
 
+import '../services/auth_service.dart';
+
 List<ChangeNotifierProvider> providers = [
-  ChangeNotifierProvider(create: (_) => LoginViewModel()),
+  ChangeNotifierProvider(create: (_) => LoginViewModel(authService: AuthService())),
 ];

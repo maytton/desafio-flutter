30618f20e489bbf8ea36ed6eb8cf3a226efb27d6
Maytton <maytton0000@gmail.com>
2025-06-30 15:39:07 -0300

    refactor(home_view): Remove widget Align e ConstrainedBox

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -35,91 +35,85 @@ class _HomeViewState extends State<HomeView> {
               child: SingleChildScrollView(
                 child: LayoutBuilder(
                   builder: (context, constraints) {
-                    return Align(
-                      alignment: Alignment.topLeft,
-                      child: ConstrainedBox(
-                        constraints: const BoxConstraints(maxWidth: 700),
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Cotar e Contratar',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: 10),
-                              SingleChildScrollView(
-                                scrollDirection: Axis.horizontal,
-                                child: Wrap(
-                                  spacing: 10,
-                                  runSpacing: 10,
-                                  children: [
-                                    MenuButton(
-                                      icon: Icons.directions_car,
-                                      label: 'Automóvel',
-                                      onTap: () {},
-                                    ),
-                                    MenuButton(
-                                      icon: Icons.home,
-                                      label: 'Residencia',
-                                      onTap: () {},
-                                    ),
-                                    MenuButton(
-                                      icon: Icons.person,
-                                      label: 'Vida',
-                                      onTap: () {},
-                                    ),
-                                    MenuButton(
-                                      icon: Icons.health_and_safety,
-                                      label: 'Acidentes Pessoais ',
-                                      onTap: () {},
-                                    ),
-                                    MenuButton(
-                                      icon: Icons.business,
-                                      label: 'Empresa',
-                                      onTap: () {},
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              SizedBox(height: 20),
-                              Text(
-                                'Minha Familia',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: 10),
-                              AddCardButton(
-                                label: 'Adicione aqui os membros da familia',
-                                onTap: () {},
-                              ),
-                              SizedBox(height: 20),
-                              Text(
-                                'Contratados',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: 10),
-                              AddCardButton(
-                                label:
-                                    'Você ainda não possui seguros contratados.',
-                                onTap: () {},
-                                icon: Icons.sentiment_dissatisfied,
-                              ),
-                            ],
+                    return Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Cotar e Contratar',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
+                          SizedBox(height: 10),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Wrap(
+                              spacing: 10,
+                              runSpacing: 10,
+                              children: [
+                                MenuButton(
+                                  icon: Icons.directions_car,
+                                  label: 'Automóvel',
+                                  onTap: () {},
+                                ),
+                                MenuButton(
+                                  icon: Icons.home,
+                                  label: 'Residencia',
+                                  onTap: () {},
+                                ),
+                                MenuButton(
+                                  icon: Icons.person,
+                                  label: 'Vida',
+                                  onTap: () {},
+                                ),
+                                MenuButton(
+                                  icon: Icons.health_and_safety,
+                                  label: 'Acidentes Pessoais ',
+                                  onTap: () {},
+                                ),
+                                MenuButton(
+                                  icon: Icons.business,
+                                  label: 'Empresa',
+                                  onTap: () {},
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'Minha Familia',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          AddCardButton(
+                            label: 'Adicione aqui os membros da familia',
+                            onTap: () {},
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'Contratados',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          AddCardButton(
+                            label:
+                                'Você ainda não possui seguros contratados.',
+                            onTap: () {},
+                            icon: Icons.sentiment_dissatisfied,
+                          ),
+                        ],
                       ),
                     );
                   },

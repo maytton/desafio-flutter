d20b9fef0aa712774675ca819ad742829786076c
Maytton <maytton0000@gmail.com>
2025-07-01 12:02:40 -0300

    feat(home): Exibe nome do usuário logado

--- lib/viewmodels/home_view_model.dart
+++ lib/viewmodels/home_view_model.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewModel extends ChangeNotifier {
+
+  String? get nomeUsuario => FirebaseAuth.instance.currentUser?.displayName;
 
 }

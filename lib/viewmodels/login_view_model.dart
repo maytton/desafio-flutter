30e4ac504d2d18ad54631e271b159f5db13cf282
Maytton <maytton0000@gmail.com>
2025-07-01 11:58:06 -0300

    feat: Criação da LoginViewModel
    Cria a ViewModel para gerenciar a lógica de login.

---
+++ lib/viewmodels/login_view_model.dart
@@ -0,0 +1,66 @@
+import 'package:desafio_flutter/services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+
+class LoginViewModel extends ChangeNotifier {
+  final AuthService _authService;
+
+  LoginViewModel({required AuthService authService})
+      : _authService = authService;
+
+  String cpf = '';
+  String password = '';
+  String nome = '';
+  bool loading = false;
+
+  void setCpf(String val) {
+    cpf = val;
+    notifyListeners();
+  }
+
+  void setPassword(String value) {
+    password = value;
+    notifyListeners();
+  }
+
+  void setNome(String val) {
+    nome = val;
+    notifyListeners();
+  }
+
+  String get _cpfComoEmail => '$cpf@meuapp.com';
+
+
+  Future<bool> login() async {
+    try {
+      loading = true;
+      notifyListeners();
+
+      await _authService.login(_cpfComoEmail, password);
+      return true;
+    } catch (e) {
+      debugPrint('Error ao realizar login: $e');
+      return false;
+    } finally {
+      loading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<bool> register() async {
+    try {
+      loading = true;
+      notifyListeners();
+
+      await _authService.register(_cpfComoEmail, password, nome: nome);
+      return true;
+    } catch (e) {
+      debugPrint('Erro: $e');
+      return false;
+    } finally {
+      loading = false;
+      notifyListeners();
+    }
+  }
+}

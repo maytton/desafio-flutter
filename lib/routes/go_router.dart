9ad40c4e895f8d2c6e0988dcb923abd5b9bc0b05
Maytton <maytton0000@gmail.com>
2025-07-01 22:10:48 -0300

    feat: routes: Adiciona rota para WebViewPage

--- lib/routes/go_router.dart
+++ lib/routes/go_router.dart
@@ -1,6 +1,7 @@
 import 'package:desafio_flutter/routes/app_routes.dart';
 import 'package:desafio_flutter/viewmodels/login_view_model.dart';
 import 'package:desafio_flutter/views/login/login_view.dart';
+import 'package:desafio_flutter/views/shared/web_view_page.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
@@ -27,6 +28,14 @@ final GoRouter router = GoRouter(
           create: (_) => HomeViewModel(),
           child: const HomeView(),
         )
+      ),
+      GoRoute(
+        path: '/webview',
+        name: 'webview',
+        builder: (context, state) {
+          final url = state.uri.queryParameters['url'] ?? 'https://flutter.dev';
+          return WebViewPage(url: url);
+        },
       ),
     ],
 );
